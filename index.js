19d7d49ee4169a3f2f190d2808cf886c07cd8531
Peter deHaan <peter@deseloper.com>
2015-08-27 14:58:37 -0700

    Tweak indent rule to handle switch case indentation

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "airbnb/base"
-  ],
-  "rules": {
-    "comma-dangle": [2, "never"],
-    "no-console": 1,
-    "no-use-before-define": [2, "nofunc"]
-  }
-};
+module.exports = require('./rules.json');
