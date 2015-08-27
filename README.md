a57fc56f89f950cc46fe51676e3e55c4fb294371
Peter deHaan <peter@deseloper.com>
2015-08-27 12:49:00 -0700

    Lets get this party started

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # eslint-config-idea-town
+
 Shared ESLint config for idea-town repos.
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "airbnb"
+  ],
+  "rules": {
+    "comma-dangle": [2, "never"],
+    "no-console": 1,
+    "no-use-before-define": [2, "nofunc"]
+  }
+};
